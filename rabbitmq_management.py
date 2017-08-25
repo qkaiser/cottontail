586015286093585221fdcbdb8cd0fce5b6aa0f2f
Quentin Kaiser <kaiserquentin@gmail.com>
2017-08-25 23:27:29 +0200

    Remove verbose warning message due to verify=False in requests.

--- rabbitmq_management.py
+++ rabbitmq_management.py
@@ -17,6 +17,8 @@ Example:
 """
 from urllib import quote
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 class UnauthorizedAccessException(Exception):
     """Custom exception for HTTP 401"""
