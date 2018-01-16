4af5b545135fb6f97d93e17cf3231391ed5f4536
Quentin Kaiser <kaiserquentin@gmail.com>
2018-01-16 17:37:10 +0100

    pip package prep.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+
+from cottontail import __version__
+
+try:
+    long_description = open( 'README.md', 'rt' ).read()
+except:
+    long_description = 'Cottontail - A set of scripts to capture RabbitMQ messages.'
+
+setup(
+    name                    = 'cottontail',
+    version                 = __version__,
+    description             = long_description,
+    long_description        = long_description,
+    author                  = 'Quentin Kaiser',
+    author_email            = 'kaiserquentin@gmail.com',
+    url                     = 'http://www.github.com/qkaiser/cottontail',
+    packages                = find_packages(),
+    include_package_data    =  False,
+    scripts                 = [ 'bin/cottontail' ],
+    license                 = 'BSD-3',
+    zip_safe                = False,
+    install_requires        = [ 'coloredlogs', 'verboselogs', 'pika',\
+        'requests', 'urllib3' ]
+)
